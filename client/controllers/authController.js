5a3fb6aa83f2c674ee63745a484930f91b47d637
RJ Almeda <rjalmeda@D-173-250-196-183.dhcp4.washington.edu>
2017-04-11 11:23:26 -0700

    auth tools working.  Check Token and Get Token

--- client/controllers/authController.js
+++ client/controllers/authController.js
@@ -17,15 +17,39 @@ app.controller('authController', function($scope, $location, authFactory){
             return console.log("Client ID?");
         } else if (!$scope.auth.clientSecret){
             return console.log("Client Secret?");
-        } else if (!$scope.auth.predixZoneId){
-            return console.log("Predix Zone Id?");
         } else if (!$scope.auth.access_token){
             return console.log("Access token?")
         } else {
             authFactory.checkAuth($scope.auth, function(data){
                 console.log(data);
+                if(data.errors){
+                    return $scope.render.status = JSON.stringify(data, null, 2);
+                }
                 $scope.render.status = JSON.stringify(data.data, null, 2);
             });
         };
     };
+    $scope.getToken = function(){
+        if(!$scope.auth){
+            return console.log("Wheres you auth?");
+        } else if (!$scope.auth.uaa){
+            return console.log("You Need A UAA");
+        } else if (!$scope.auth.clientId){
+            return console.log("Client ID?");
+        } else if (!$scope.auth.clientSecret){
+            return console.log("Client Secret?");
+        } else if (!$scope.auth.predixZoneId){
+            return console.log("Predix Zone Id?");
+        } else {
+            authFactory.getToken($scope.auth, function(data){
+                console.log(data);
+                if(data.errors){
+                    return $scope.render.status = JSON.stringify(data, null, 2);
+                }
+                $scope.render.status = JSON.stringify(data.data, null, 2);
+                $scope.auth.access_token = data.data.access_token;
+                $scope.render.newAuth = JSON.stringify($scope.auth, null, 2);
+            });
+        };
+    }
 });
--- client/factories/authFactory.js
+++ client/factories/authFactory.js
@@ -14,6 +14,26 @@ app.factory('authFactory', function($http){
 //        return callback({config: config, requestBody: requestBody})
         $http.post(auth.uaa+'/check_token'+"?token="+auth.access_token, requestBody, config).then(function(data){
             callback(data);
+        }).catch(function(e){
+            callback({errors: e});
+        });
+    };
+    
+    factory.getToken = function(auth, callback){
+        var passwordString = auth.clientId+':'+auth.clientSecret;
+        var base64password = btoa(passwordString);
+        var config = {
+            headers: {
+                Authorization: "Basic "+base64password,
+                "content-type": "application/x-www-form-urlencoded"
+            }
+        };
+        var requestBody = {token:auth.access_token};
+//        return callback({config: config, requestBody: requestBody})
+        $http.get(auth.uaa+'/oauth/token'+"?grant_type=client_credentials", config).then(function(data){
+            callback(data);
+        }).catch(function(e){
+            callback({errors: e})
         });
     };
     return factory;
